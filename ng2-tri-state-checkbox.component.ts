3d0e759d427cf09bfe5ae8e966f61e465da50037
Sebastien de Salvador <sebastien.desalvador@gmail.com>
2017-02-27 17:28:39 +0100

    Initial upload

---
+++ ng2-tri-state-checkbox.component.ts
@@ -0,0 +1,94 @@
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  Input,
+  EventEmitter,
+  OnChanges,
+  OnInit,
+  forwardRef
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  moduleId: module.id,
+  selector: 'ng2-tri-state-checkbox',
+  template: `<input #checkbox type="checkbox" (blur)="onBlur()" (click)="setState()" />`,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => Ng2TriStateCheckboxComponent),
+      multi: true
+    }
+  ]
+})
+export class Ng2TriStateCheckboxComponent implements OnChanges, OnInit, ControlValueAccessor {
+
+  get value() {
+    return this._value;
+  }
+  set value(val) {
+    this._value = val;
+    this.onChangeCallback(this._value);
+  }
+
+  @Input() private _value: boolean = null;
+  @ViewChild('checkbox') private _checkbox: ElementRef;
+
+  public ngOnChanges(): void {
+    this.render();
+  }
+
+  public ngOnInit(): void {
+    this.render();
+  }
+
+  public writeValue(value: any): void {
+    if (value !== undefined) {
+      this.value = value;
+    }
+  }
+
+  public registerOnChange(fn): void {
+    this.onChangeCallback = fn;
+  }
+
+  public registerOnTouched(fn): void {
+    this.onTouchedCallback = fn;
+  }
+
+  public onBlur(): void {
+    this.onTouchedCallback();
+  }
+
+  public setState(): void {
+    if (this._checkbox.nativeElement.readOnly) {
+      this._checkbox.nativeElement.checked = this._checkbox.nativeElement.readOnly = this.value = false;
+    } else if (!this._checkbox.nativeElement.checked) {
+      this._checkbox.nativeElement.readOnly = this._checkbox.nativeElement.indeterminate = true;
+      this.value = null;
+    } else {
+      this.value = true;
+    }
+  }
+
+  private onChangeCallback = (_: any) => { };
+  private onTouchedCallback = () => { };
+
+  private render(): void {
+    switch(this.value) {
+      case true:
+        this._checkbox.nativeElement.readOnly = this._checkbox.nativeElement.indeterminate = false;
+        this._checkbox.nativeElement.checked = true;
+        break;
+      case false:
+        this._checkbox.nativeElement.checked = this._checkbox.nativeElement.readOnly = false;
+        break;
+      case null:
+      case undefined:
+      default:
+        this._checkbox.nativeElement.readOnly = this._checkbox.nativeElement.indeterminate = true;
+        break;
+    }
+  }
+}
